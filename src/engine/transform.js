fac166b116986ced0d9ec57ea5e260195752da8d
Baransu <sajgonek123@gmail.com>
2016-02-18 20:38:33 +0100

    transform rotation fix

--- src/engine/transform.js
+++ src/engine/transform.js
@@ -3,7 +3,7 @@ window.Transform = (function() {
     var Transform = function Transform(args) {
       this.position = args.position || new Vec2();
       this.scale = args.scale || new Vec2(1, 1);
-      this.rotation = args.rotation || 0;
+      this.rotation = Number(args.rotation) || 0;
 
     };
 
